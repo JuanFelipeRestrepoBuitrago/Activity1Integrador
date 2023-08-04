7a2701c45273a3395ffedb3df7225c0f2d2a06df
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-08-03 20:25:10 -0500

    Movies Visualization

--- movie/models.py
+++ movie/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=1000)
+    image = models.ImageField(upload_to='./images/')
+    url = models.URLField(blank=True)
--- movie/views.py
+++ movie/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Movie
+
 # Create your views here.
 def home(request):
+    # return render(request, "home.html", {
+    #     "name": "Juan Felipe Restrepo"
+    # })
+    search_term = request.GET.get('searchMovie')
+    movies = Movie.objects.all()
+    if search_term:
+        movies = movies.filter(title__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
     return render(request, "home.html", {
-        "name": "Juan Felipe Restrepo"
+        "search_term": search_term,
+        "movies": movies
     })
 
 def about(request):
