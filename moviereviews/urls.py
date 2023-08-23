9fe94c91675a92403a44f7f81ced65915e56afa0
Juan Felipe Restrepo Buitrago <jfrestrepb@eafit.edu.co>
2023-08-23 10:52:05 -0500

    Show  News

--- moviereviews/urls.py
+++ moviereviews/urls.py
@@ -23,6 +23,7 @@ from django.conf.urls.static import static # new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", include("movie.urls")),
+    path("news/", include("news.urls")),
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # new
